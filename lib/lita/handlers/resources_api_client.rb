eabc98da97ba35e08bbf0251c5c0ffda620386a8
Gabi Jack <gabi@ministrycentered.com>
2019-08-20 10:18:28 -0400

    Update template

--- lib/lita/handlers/resources_api_client.rb
+++ lib/lita/handlers/resources_api_client.rb
@@ -10,7 +10,7 @@ module Lita
       route(/list\s+approval\s+groups/i, :respond_with_approval_groups, command: true)
 
       def respond_with_approval_groups(response)
-        response.reply(MultiJson.dump(formatted_approval_groups))
+        response.reply(formatted_approval_groups)
       end
 
       def api
