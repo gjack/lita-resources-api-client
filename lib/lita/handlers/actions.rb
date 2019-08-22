f77111008214ac95653e6f902e83ac84feb730a3
Gabi Jack <gabi@ministrycentered.com>
2019-08-21 20:00:22 -0400

    Fix typo in method name

--- lib/lita/handlers/actions.rb
+++ lib/lita/handlers/actions.rb
@@ -1,7 +1,7 @@
 module Lita
   module Handlers
     class Actions < Handler
-      on :approval_group_selection, :subcribe_to_pending_approvals
+      on :approval_group_selection, :subscribe_to_pending_approvals
 
       def subscribe_to_pending_approvals(payload)
         Lita.logger.info "#{payload}"
